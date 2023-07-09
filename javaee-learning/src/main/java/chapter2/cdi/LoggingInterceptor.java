32abe38f61e1cc78e2d2d80191cd3941af80ae47
Francesco Maria D'Auria <dauriafrancescomaria@gmail.com>
2023-07-09 15:34:26 +0200

    Create LoggingInterceptor.java

---
+++ javaee-learning/src/main/java/chapter2/cdi/LoggingInterceptor.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chapter2.cdi;
+
+import java.util.logging.Logger;
+import javax.inject.Inject;
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.Interceptor;
+import javax.interceptor.InvocationContext;
+
+
+/**
+ *
+ * @author dauri
+ */
+
+@Interceptor
+@Loggable
+public class LoggingInterceptor {
+    
+    
+    @Inject
+    private Logger logger;
+    
+    @AroundInvoke
+    public Object logMethod(InvocationContext ic) throws Exception{
+        logger.entering(ic.getTarget().getClass().getName(), ic.getMethod().getName());
+        
+        try{
+            return ic.proceed();
+        }
+        finally{
+            logger.exiting(ic.getTarget().getClass().getName(), ic.getMethod().getName());
+        }
+    }
+    
+}
