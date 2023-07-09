5442d0be646924a6c8432fb1e18ae51c248199fc
Francesco Maria D'Auria <dauriafrancescomaria@gmail.com>
2023-07-09 20:55:54 +0200

    Added main (to test with Weld)

---
+++ javaee-learning/src/main/java/chapter2/cdi/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chapter2.cdi;
+
+/**
+ *
+ * @author dauri
+ */
+public class Main {
+    
+    public static void main(String[] args){
+        
+        Weld weld = new Weld();
+    }
+}
