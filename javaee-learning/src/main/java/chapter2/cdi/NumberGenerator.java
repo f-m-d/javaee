2869600fcf6ab926a540304f49067a372e9e7f5b
Francesco Maria D'Auria <dauriafrancescomaria@gmail.com>
2023-07-09 14:54:07 +0200

    Number Generator interface created

---
+++ javaee-learning/src/main/java/chapter2/cdi/NumberGenerator.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package chapter2.cdi;
+
+/**
+ *
+ * @author dauri
+ */
+public interface NumberGenerator {
+    
+    String generateNumber();
+    
+}
