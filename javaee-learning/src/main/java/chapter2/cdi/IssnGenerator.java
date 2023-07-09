99c561b173c653bc027523269bf601d4dd1a6439
Francesco Maria D'Auria <dauriafrancescomaria@gmail.com>
2023-07-09 15:16:43 +0200

    Create IssnGenerator.java

---
+++ javaee-learning/src/main/java/chapter2/cdi/IssnGenerator.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chapter2.cdi;
+
+import java.util.Random;
+import java.util.logging.Logger;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author dauri
+ */
+@EightDigits
+public class IssnGenerator implements NumberGenerator{
+
+    @Inject
+    private Logger logger;
+    
+    @Override
+    public String generateNumber() {
+    //    throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    String issn = "8-" + Math.abs(new Random().nextInt());
+    logger.info("Generated ISSN: " + issn);
+    return issn;
+    }
+    
+    
+    
+    
+}
