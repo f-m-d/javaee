a36e242edeaf0b68f77e223f8864a28f63ddac86
Francesco Maria D'Auria <dauriafrancescomaria@gmail.com>
2023-07-09 15:16:42 +0200

    Create IsbnGenerator.java

---
+++ javaee-learning/src/main/java/chapter2/cdi/IsbnGenerator.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chapter2.cdi;
+
+import java.util.Random;
+import java.util.logging.Logger;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author dauri
+ */
+
+@ThirteenDigits
+public class IsbnGenerator implements NumberGenerator{
+
+    @Inject
+    private Logger logger;
+
+
+    public String generateNumber() {
+    //    throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    String isbn = "13-84356" + Math.abs(new Random().nextInt());
+    logger.info("Generated ISBN: " + isbn);
+    return isbn;
+    }
+    
+    
+    
+    
+    
+    
+    
+}
