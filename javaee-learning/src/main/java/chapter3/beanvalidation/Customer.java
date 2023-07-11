8c7c2767624ba7746d12cac814e66d295d8c4edc
Francesco Maria D'Auria <dauriafrancescomaria@gmail.com>
2023-07-11 18:59:03 +0200

    Added Customer POJO class with Constraints

---
+++ javaee-learning/src/main/java/chapter3/beanvalidation/Customer.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chapter3.beanvalidation;
+import java.util.Date;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author dauri
+ */
+public class Customer {
+    
+    @NotNull @Size(min = 2)
+    private String firstName;
+    private String lastName;
+    @CustomEmailConstraint
+    private String email;
+    private String phoneNumber;
+    @Past
+    private Date birthDate;
+    private Address deliveryAddress;
+
+    public Customer() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Address getDeliveryAddress() {
+        return deliveryAddress;
+    }
+
+    public void setDeliveryAddress(Address deliveryAddress) {
+        this.deliveryAddress = deliveryAddress;
+    }
+    
+    
+    
+    
+}
