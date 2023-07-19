0cac0635a4b5f0a05ca7621b47214b7293e7f70d
Francesco Maria D'Auria <dauriafrancescomaria@gmail.com>
2023-07-19 15:27:33 +0200

    Corrected PU name for EntityManager creation
    Adjusted the name with the "CHAPTER04PU", the same name of the Persistence Unit written in the "persistence.xml" file present in "src/main/resources/META-INF"

--- javaee-learning/src/test/java/chapter4/javapersistanceapi/test/BookForJPAIT.java
+++ javaee-learning/src/test/java/chapter4/javapersistanceapi/test/BookForJPAIT.java
@@ -10,6 +10,11 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+import chapter4.javapersistanceapi.BookForJPA;
+import javax.validation.ConstraintViolationException;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  *
@@ -17,7 +22,7 @@ import org.junit.Before;
  */
 public class BookForJPAIT {
 
-    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("CHAPTER4TESTPU");
+    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("CHAPTER04PU");
     private static EntityManager em;
     private static EntityTransaction et;
 
@@ -34,7 +39,55 @@ public class BookForJPAIT {
         }
     }
         
-        
+    
+    @Test
+    public void shouldFindJavaEE7Book() throws Exception{
+        BookForJPA book = em.find(BookForJPA.class, 1001L);
+        assertEquals("Beginning of Java EE 7", book.getTitle());
+    }
+    
+    
+//    @Test
+//    public void shouldCreateH2G2Book() throws Exception{
+//        // Create an instance of BookForJPA
+//        BookForJPA b = new BookForJPA();
+//        b.setTitle("H2G2");
+//        b.setDescription("A Famous cookbook for Java EE learning!!");
+//        b.setPrice(12.5F);
+//        b.setIsbn("1-84023-742-2");
+//        b.setNbOfPages(354);
+//        b.setIllustrations(false);
+//        
+//        
+//        //Start EntityTransaction, persist the book and close all
+//        et.begin();
+//        em.persist(b);
+//        et.commit();
+//        
+//        assertNotNull("Id should not be null", b.getId());
+//        
+//        
+//        // Retrieves all the books from database
+//        b = em.createNamedQuery("findBookH2G2", BookForJPA.class).getSingleResult();
+//        
+//        assertEquals("A Famous cookbook for Java EE learning!!", b.getDescription());
+//    }
+//    
+//    
+//    @Test(expected = ConstraintViolationException.class)
+//    public void shouldRaiseConstraintViolationExceptionNullTitle(){
+//        BookForJPA b = new BookForJPA();
+//        // It will rais an exception for this one
+//        //b.setTitle("H2G2");
+//        b.setDescription("A Famous cookbook for Java EE learning!!");
+//        b.setPrice(12.5F);
+//        b.setIsbn("1-84023-742-2");
+//        b.setNbOfPages(354);
+//        b.setIllustrations(false);
+//        
+//        em.persist(b);
+//    }
+//    
     }
 
 
