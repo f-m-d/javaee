5a8aaa174c3f053985c1bdd693f39f1b76476649
Francesco Maria D'Auria <dauriafrancescomaria@gmail.com>
2023-07-19 15:26:12 +0200

    Adjusted insert.sql file
    The problems were:
    
    The file was not positioned in the correct path (src/main/resources instead of src/main/resources/META-INF)
    
    The colums names are, by default, the same name of the variables written in the Java class (BookForJPA). It was "NBOFPAGES" instead of "NBOFPAGE"

---
+++ javaee-learning/src/main/resources/insert.sql
@@ -0,0 +1,13 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  dauri
+ * Created: Jul 14, 2023
+ */
+
+
+INSERT INTO BOOKFORJPA(ID,TITLE,DESCRIPTION,ILLUSTRATIONS,ISBN,NBOFPAGES,PRICE) VALUES (1000,'Beginning of Java EE 6', 'Best Java EE 6 BOOK EVER!!!',1, '1234-5678',450,49)
+INSERT INTO BOOKFORJPA(ID,TITLE,DESCRIPTION,ILLUSTRATIONS,ISBN,NBOFPAGES,PRICE) VALUES (1001,'Beginning of Java EE 7', 'Best Java EE 7 BOOK EVER!!!',1, '5678-9012',550,53)
+INSERT INTO BOOKFORJPA(ID,TITLE,DESCRIPTION,ILLUSTRATIONS,ISBN,NBOFPAGES,PRICE) VALUES (1002,'Lord of the Rings', 'Yep, Lord of the Rings is the true JAVA EE magic!',0, '9012-3456',222,23)
