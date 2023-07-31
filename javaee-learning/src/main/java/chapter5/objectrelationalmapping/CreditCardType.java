9dccab1bf4152d5479f1e280ef4b330ceab39ce3
Francesco Maria D'Auria <dauriafrancescomaria@gmail.com>
2023-07-31 20:27:19 +0200

    Create CreditCardType.java
    Simple enumeration created

---
+++ javaee-learning/src/main/java/chapter5/objectrelationalmapping/CreditCardType.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package chapter5.objectrelationalmapping;
+
+/**
+ *
+ * @author dauri
+ */
+public enum CreditCardType {
+    VISA,
+    MASTER_CARD,
+    AMERICAN_EXPRESS
+}
