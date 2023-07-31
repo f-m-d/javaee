963651c55e9120dbdf2ddad6458d9be6b1248a29
Francesco Maria D'Auria <dauriafrancescomaria@gmail.com>
2023-07-31 20:20:59 +0200

    Create Track.java
    The @Basic annotation is used by default for the parameters to be hold in the database: in our case, we indicate to "LAZY" so the data will be loaded when directly asked for it
    
    @Lob is used for Large Obejct Blobs, aka anything large as documents, audio files, large strings etc.
    
    @Column property can specify a large amount of info for a property in the database column mapping

---
+++ javaee-learning/src/main/java/chapter5/objectrelationalmapping/Track.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chapter5.objectrelationalmapping;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+/**
+ *
+ * @author dauri
+ */
+@Entity
+public class Track {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private Float duration;
+    @Basic(fetch = FetchType.LAZY)
+    @Lob
+    private byte[] wav;
+    @Column( name = "description_column", nullable = false, unique = false)
+    private String description;
+}
